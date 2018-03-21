d9b1bed9a5a6221c53137da27b54a325fd8bc1cc
Trey Shugart <treshugart@gmail.com>
2018-03-21 22:24:01 +1100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+const flowCopySource = require("flow-copy-source");
+const fs = require("fs-extra");
+const path = require("path");
+const uppercamelcase = require("uppercamelcase");
+const webpack = require("webpack");
+const webpackNodeExternals = require("webpack-node-externals");
+const yargs = require("yargs").argv;
+
+async function cwdPath(...parts) {
+  const possiblePath = path.join(process.cwd(), ...parts);
+  return (await fs.exists(possiblePath)) ? possiblePath : null;
+}
+
+async function cwdRequire(file) {
+  file = await cwdPath(file);
+  return file ? require(file) : null;
+}
+
+async function cwdRequireJson(file) {
+  file = await cwdPath(file);
+  return file ? JSON.parse(await fs.readFile(file)) : null;
+}
+
+(async function() {
+  const pkg = (await cwdRequireJson("package.json")) || {};
+  const opt = {
+    ...(await cwdRequire("zeropack.js")),
+    ...(await cwdRequireJson(".zeropackrc")),
+    ...pkg.zeropack,
+    ...{
+      entry: "./src/index.js",
+      externals: webpackNodeExternals(),
+      mode: yargs.mode || "production",
+      module: {
+        rules: [{ test: /\.js$/, use: "babel-loader" }]
+      },
+      output: {
+        filename: path.basename(pkg.main || "index.js"),
+        library: pkg.name ? uppercamelcase(pkg.name) : undefined,
+        libraryTarget: "umd",
+        path: path.join(process.cwd(), path.dirname(pkg.main || "dist"))
+      }
+    }
+  };
+
+  await fs.remove(opt.output.path);
+
+  webpack(opt, (error, stats) => {
+    if (error) {
+      console.error(error.stack || error);
+      console.error(error.details);
+    } else if (stats.hasErrors() || stats.hasWarnings()) {
+      const info = stats.toJson();
+      console.warn(info.warnings);
+      console.error(info.errors);
+    } else {
+      const sources = Array.isArray(opt.entry)
+        ? Object.values(opt.entry)
+        : [opt.entry];
+      flowCopySource(sources.map(path.dirname), opt.output.path, {
+        ignore: "**/__tests__/**"
+      });
+    }
+  });
+})();
