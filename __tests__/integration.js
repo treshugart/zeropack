8d1e6a5388b4b6a41e26a3a6f5c250176cc9e976
Trey Shugart <treshugart@gmail.com>
2018-03-22 22:41:03 +1100

    Add travis file and prepare for babel builds.

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -14,4 +14,8 @@ test("custom", async () => {
   await run("custom");
   expect(await read("dist/main.js")).toMatchSnapshot();
   expect(await read("dist/main.js.flow")).toMatchSnapshot();
+  // expect(await read("dist/module.js")).toMatchSnapshot();
+  // expect(await read("dist/module.js.flow")).toMatchSnapshot();
+  // expect(await read("dist/esnext.js")).toMatchSnapshot();
+  // expect(await read("dist/esnext.js.flow")).toMatchSnapshot();
 });
