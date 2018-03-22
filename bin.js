bdfac6e50299544a9e4bc6f88fe216e9073fc346
Trey Shugart <treshugart@gmail.com>
2018-03-22 16:25:09 +1100

    Fix bin script.

--- bin.js
+++ bin.js
@@ -2,5 +2,6 @@ const yargs = require("yargs");
 const { zeropack } = require(".");
 
 (async function() {
-  await zeropack(yargs.argv);
+  const { _, $0, help, version, ...args } = yargs.argv;
+  await zeropack(args);
 })();
