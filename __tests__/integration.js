67ea28a1059c4e51870b07bcddbe1c8ae3534c96
Trey Shugart <treshugart@gmail.com>
2018-03-24 15:51:14 +1100

    Update snapshots.

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -12,10 +12,10 @@ test("defaults - { mode: development }", async () => {
 
 test("custom", async () => {
   await run("custom");
+  expect(await read("dist/browser.js")).toMatchSnapshot();
+  expect(await read("dist/browser.js.flow")).toMatchSnapshot();
   expect(await read("dist/main.js")).toMatchSnapshot();
   expect(await read("dist/main.js.flow")).toMatchSnapshot();
-  // expect(await read("dist/module.js")).toMatchSnapshot();
-  // expect(await read("dist/module.js.flow")).toMatchSnapshot();
-  // expect(await read("dist/esnext.js")).toMatchSnapshot();
-  // expect(await read("dist/esnext.js.flow")).toMatchSnapshot();
+  expect(await read("dist/module.js")).toMatchSnapshot();
+  expect(await read("dist/module.js.flow")).toMatchSnapshot();
 });
