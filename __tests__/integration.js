454bdee022782cf8a72460e231c2c2cd3c0059f2
Trey Shugart <treshugart@gmail.com>
2018-03-24 15:53:13 +1100

    Update default tests and snapshots.

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -3,19 +3,18 @@ const { read, run } = require("./__utils__");
 test("defaults", async () => {
   await run("defaults");
   expect(await read("dist/index.js")).toMatchSnapshot();
-});
-
-test("defaults - { mode: development }", async () => {
-  await run("defaults", "--mode", "development");
-  expect(await read("dist/index.js")).toMatchSnapshot();
+  expect(await read("dist/index.js.map")).toMatchSnapshot();
 });
 
 test("custom", async () => {
   await run("custom");
   expect(await read("dist/browser.js")).toMatchSnapshot();
   expect(await read("dist/browser.js.flow")).toMatchSnapshot();
+  expect(await read("dist/browser.js.map")).toMatchSnapshot();
   expect(await read("dist/main.js")).toMatchSnapshot();
   expect(await read("dist/main.js.flow")).toMatchSnapshot();
+  expect(await read("dist/main.js.map")).toMatchSnapshot();
   expect(await read("dist/module.js")).toMatchSnapshot();
   expect(await read("dist/module.js.flow")).toMatchSnapshot();
+  expect(await read("dist/module.js.map")).toMatchSnapshot();
 });
