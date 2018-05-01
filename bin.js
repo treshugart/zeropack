a7016090ec9256e33a3ac63921a5d0029e6b3d98
Trey Shugart <treshugart@gmail.com>
2018-05-01 13:22:36 +1000

    Update source-trace.

--- bin.js
+++ bin.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
+const nodeWatch = require("node-watch");
 const yargs = require("yargs");
 const { zeropack } = require(".");
 
 (async function() {
-  const { _, $0, help, version, ...args } = yargs.argv;
-  await zeropack(args);
+  const { _, $0, help, version, watch, ...args } = yargs.argv;
+  if (watch) {
+    nodeWatch(".", { recursive: true }, async () => await zeropack(args));
+  } else {
+    await zeropack(args);
+  }
 })();
